52667148c0d55967d7aae6079408e86597d677ef
heysvtya <132353944+heysvtya@users.noreply.github.com>
2023-05-02 17:03:16 +0530

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,12 @@
+//This that kinda beat that go ratata
+#include<stdio.h>
+int main() 
+{
+float s, e, x ;
+printf("Type two dumbass numbers bozo");
+scanf("%f",&s);
+scanf("%f",&e);
+x=s+e;
+printf("here's your answer=%f",x);
+getch() 
+}
